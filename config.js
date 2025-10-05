df50dea9bc5594c072a2af9d56c37d8196824a32
happyfamily9951-source <happyfamily9951@gmail.com>
2025-10-05 11:59:40 +1100

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 // config.js
-const API_URL = "https://script.google.com/macros/s/AKfycbywt8Y5aqs-NwVU1L-FT_zB4BZXal7Kq3OfwHFkeAp5HyLS8XFit1DIj7F1oifusdBJ/exec";
+// const API_URL = "https://script.google.com/macros/s/AKfycbywt8Y5aqs-NwVU1L-FT_zB4BZXal7Kq3OfwHFkeAp5HyLS8XFit1DIj7F1oifusdBJ/exec";
+const API_URL = "https://tight-rain-0894.happyfamily9951.workers.dev/";
